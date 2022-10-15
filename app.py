fd45c76540242625290a5a7dcb42b1f056b545db
Ivykalegi <ivykalegi@gmail.com>
2022-10-15 11:45:10 +0100

    add basic timer with Javascript code

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template
 
 
-from datetime import datetime
-
-now = datetime.now()
-
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-
 app = Flask(__name__)
 
+
 @app.get("/")
-def hello():
-    return current_time
+def timer():
+    return render_template("timer.html")
 
 
 if __name__ == "__main__":
